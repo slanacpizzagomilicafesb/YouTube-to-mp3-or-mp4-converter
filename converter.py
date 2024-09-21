d4c0f4f2839fc65731fb5ba4d7d2bdb22d259cad
slanacpizzagomilicafesb <zvukoj00@fesb.hr>
2024-09-21 20:01:42 +0200

    Most of the UI done

--- converter.py
+++ converter.py
@@ -1,15 +1,53 @@
 import tkinter as tk
 import tkinter.ttk as ttk
+from pytube import YouTube
+from moviepy.editor import *
 
 window = tk.Tk()
 window.title("YT to mp3 converter")
+save_path = "D:/Music/converter_music/"
 
 mp3_flag = 1
 mp4_flag = 0
 
 def convert_command():
-    print()
+    if mp3_flag == 1:
+        url = url_entry.get()
+        # Use pytube to download the video
+        yt = YouTube(url)
+        video = yt.streams.filter(only_audio=True).first()
+        
+        # Download the video to a file
+        out_file = video.download(output_path=save_path)
+        
+        # Set up new filename for the MP3
+        new_file = out_file.replace(".mp4", ".mp3")
+        # Use moviepy to convert video audio to mp3
+        video_clip = AudioFileClip(out_file)
+        video_clip.write_audiofile(new_file)
+        
+        # Remove the original video file
+        os.remove(out_file)
+        
+        return new_file  # Return the path to the new file
+"""
+    elif mp4_flag == 1:
+        url = url_entry.get()
+        video_info = youtube_dl.YoutubeDL().extract_info(
+            url = url,download=False
+        )
+        filename = f"{video_info['title']}.mp4"
+        options={
+            'format':'bestaudio/best',
+            'keepvideo':False,
+            'outtmpl':filename,
+        }
 
+        with youtube_dl.YoutubeDL(options) as ydl:
+            ydl.download([video_info['webpage_url']])
+
+        print("Download complete... {}".format(filename))
+"""
 def mp3_command():
     mp3_flag = 1
     mp4_flag = 0
@@ -36,7 +74,7 @@ url_entry = tk.Entry(master=url_entry_frame,
                      )
 url_entry.pack(side=tk.RIGHT)
 
-url = url_entry.get()
+
 
 mp3_btn = tk.Button(master=mp3_4_btn_frame,
                     text=".mp3",
