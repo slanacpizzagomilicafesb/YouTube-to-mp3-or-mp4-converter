75da87bfb04aa25942f162f00b5c91ffea40120e
slanacpizzagomilicafesb <zvukoj00@fesb.hr>
2024-09-24 20:25:37 +0200

    mp4 sorta works, added folder checkbox which actually works

--- converter.py
+++ converter.py
@@ -2,55 +2,106 @@ import tkinter as tk
 import tkinter.ttk as ttk
 from pytube import YouTube
 from moviepy.editor import *
+from tkinter.filedialog import askdirectory
 
 window = tk.Tk()
 window.title("YT to mp3 converter")
 save_path = "D:/Music/converter_music/"
 
-mp3_flag = 1
-mp4_flag = 0
-
 def convert_command():
-    if mp3_flag == 1:
-        url = url_entry.get()
-        # Use pytube to download the video
-        yt = YouTube(url)
-        video = yt.streams.filter(only_audio=True).first()
-        
-        # Download the video to a file
-        out_file = video.download(output_path=save_path)
-        
-        # Set up new filename for the MP3
-        new_file = out_file.replace(".mp4", ".mp3")
-        # Use moviepy to convert video audio to mp3
-        video_clip = AudioFileClip(out_file)
-        video_clip.write_audiofile(new_file)
-        
-        # Remove the original video file
-        os.remove(out_file)
-        
-        return new_file  # Return the path to the new file
-"""
-    elif mp4_flag == 1:
-        url = url_entry.get()
-        video_info = youtube_dl.YoutubeDL().extract_info(
-            url = url,download=False
-        )
-        filename = f"{video_info['title']}.mp4"
-        options={
-            'format':'bestaudio/best',
-            'keepvideo':False,
-            'outtmpl':filename,
-        }
+    if mp3_flag.get() == 1:
+        if default_folder.get() == 1:
+            url = url_entry.get()
+            # Use pytube to download the video
+            yt = YouTube(url)
+            video = yt.streams.filter(only_audio=True).first()
+            
+            # Download the video to a file
+            out_file = video.download(output_path=save_path)
+            
+            # Set up new filename for the MP3
+            new_file = out_file.replace(".mp4", ".mp3")
+            # Use moviepy to convert video audio to mp3
+            video_clip = AudioFileClip(out_file)
+            video_clip.write_audiofile(new_file)
+            
+            # Remove the original video file
+            os.remove(out_file)
+            
+            url_entry.delete(0, tk.END)
 
-        with youtube_dl.YoutubeDL(options) as ydl:
-            ydl.download([video_info['webpage_url']])
+            # return new_file  # Return the path to the new file
+        elif default_folder.get() == 0:
+            new_save_path = askdirectory()
+            url = url_entry.get()
+            # Use pytube to download the video
+            yt = YouTube(url)
+            video = yt.streams.filter(only_audio=True).first()
+            
+            # Download the video to a file
+            out_file = video.download(output_path=new_save_path)
+            
+            # Set up new filename for the MP3
+            new_file = out_file.replace(".mp4", ".mp3")
+            # Use moviepy to convert video audio to mp3
+            video_clip = AudioFileClip(out_file)
+            video_clip.write_audiofile(new_file)
+            
+            # Remove the original video file
+            os.remove(out_file)
+            
+            url_entry.delete(0, tk.END)
 
-        print("Download complete... {}".format(filename))
-"""
+            # return new_file  # Return the path to the new file
+
+    elif mp4_flag.get() == 1:
+        if default_folder.get() == 1:
+            url = url_entry.get()
+            # Use pytube to download the video
+            yt = YouTube(url)
+            video = yt.streams.filter(only_audio=False).first()
+            
+            # Download the video to a file
+            out_file = video.download(output_path=save_path)
+            
+            # Set up new filename for the MP3
+            #new_file = out_file.replace(".mp4", ".mp3")
+            # Use moviepy to convert video audio to mp3
+            video_clip = VideoFileClip(out_file)
+            video_clip.write_videofile(out_file)
+            
+            # Remove the original video file
+            #os.remove(out_file)
+            
+            url_entry.delete(0, tk.END)
+
+        elif default_folder.get() == 0:
+            new_save_path = askdirectory()
+            url = url_entry.get()
+            # Use pytube to download the video
+            yt = YouTube(url)
+            video = yt.streams.filter(only_audio=False).first()
+            
+            # Download the video to a file
+            out_file = video.download(output_path=new_save_path)
+            
+            # Set up new filename for the MP3
+            #new_file = out_file.replace(".mp4", ".mp3")
+            # Use moviepy to convert video audio to mp3
+            #video_clip = AudioFileClip(out_file)
+            #video_clip.write_audiofile(new_file)
+            
+            # Remove the original video file
+            #os.remove(out_file)
+            
+            url_entry.delete(0, tk.END)
+
+mp3_flag = tk.IntVar(window, value=1, name="mp3")
+mp4_flag = tk.IntVar(window, value=0, name="mp4")
+
 def mp3_command():
-    mp3_flag = 1
-    mp4_flag = 0
+    window.setvar(name = "mp3", value=1)
+    window.setvar(name = "mp4", value=0)
     mp3_btn["relief"] = tk.SUNKEN
     mp4_btn["relief"] = tk.RAISED
     mp4_btn["borderwidth"] = 2
@@ -58,21 +109,25 @@ def mp3_command():
  
 
 def mp4_command():
-    mp4_flag = 1
-    mp3_flag = 0
+    window.setvar(name = "mp4", value=1)
+    window.setvar(name = "mp3", value=0)
     mp4_btn["relief"] = tk.SUNKEN
     mp3_btn["relief"] = tk.RAISED
     mp4_btn["borderwidth"] = 5
     mp3_btn["borderwidth"] = 2
 
-url_entry_frame = tk.Frame()
-convert_btn_frame = tk.Frame()
-mp3_4_btn_frame = tk.Frame()
+def default_folder_command():
+    print(default_folder.get())
 
-url_entry = tk.Entry(master=url_entry_frame,
+url_entry_convert_btn_frame = tk.Frame()
+#convert_btn_frame = tk.Frame()
+mp3_4_btn_frame = tk.Frame()
+default_folder_frame = tk.Frame()
+
+url_entry = tk.Entry(master=url_entry_convert_btn_frame,
                      width=50
                      )
-url_entry.pack(side=tk.RIGHT)
+url_entry.pack(side=tk.LEFT)
 
 
 
@@ -94,18 +149,28 @@ mp4_btn = tk.Button(master=mp3_4_btn_frame,
                     )
 mp4_btn.pack(side=tk.LEFT)
 
-convert_btn = tk.Button(master=convert_btn_frame,
+convert_btn = tk.Button(master=url_entry_convert_btn_frame,
                         text="Convert",
                         width=10,
                         height=1,
                         command=convert_command
                         )
-convert_btn.pack(side=tk.TOP)
+convert_btn.pack(side=tk.RIGHT)
 
+default_folder = tk.IntVar(value=1)
+default_folder_btn = ttk.Checkbutton(master=default_folder_frame,
+                                     text="Default folder",
+                                     variable=default_folder,
+                                     onvalue=1,
+                                     offvalue=0,
+                                     command=default_folder_command
+                                     )
+default_folder_btn.pack(side=tk.RIGHT)
 
-url_entry_frame.pack(side=tk.TOP)
-convert_btn_frame.pack(side=tk.RIGHT)
-mp3_4_btn_frame.pack()
+url_entry_convert_btn_frame.pack(side=tk.TOP)
+#convert_btn_frame.pack(side=tk.RIGHT)
+mp3_4_btn_frame.pack(side=tk.LEFT)
+default_folder_frame.pack(side=tk.RIGHT)
 
 # Start the event loop.
 window.mainloop()
