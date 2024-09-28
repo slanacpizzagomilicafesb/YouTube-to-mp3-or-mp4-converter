c35dc82a43b03443259ef0877e119e4922a842eb
slanacpizzagomilicafesb <zvukoj00@fesb.hr>
2024-09-28 21:41:54 +0200

    added radiobuttons for resolutions when downloading mp4 files, also added error exceptions

--- converter.py
+++ converter.py
@@ -5,7 +5,6 @@ from pytubefix.cli import on_progress
 from moviepy.editor import *
 from tkinter.filedialog import askdirectory
 import moviepy.editor as mpe
-import string
 
 window = tk.Tk()
 window.title("YT to mp3 converter")
@@ -14,100 +13,126 @@ save_path = "D:/Music/converter_music/"
 def convert_command():
     if mp3_flag.get() == 1:
         if default_folder.get() == 1:
-            url = url_entry.get()
             # Use pytube to download the video
-            yt = YouTube(url)
-            video = yt.streams.filter(only_audio=True).first()
-            
-            # Download the video to a file
-            out_file = video.download(output_path=save_path)
-            
-            # Set up new filename for the MP3
-            new_file = out_file.replace(".mp4", ".mp3")
-            # Use moviepy to convert video audio to mp3
-            video_clip = AudioFileClip(out_file)
-            video_clip.write_audiofile(new_file)
-            
-            # Remove the original video file
-            os.remove(out_file)
-            
-            url_entry.delete(0, tk.END)
+            yt = YouTube(url_entry.get())
+
+            try:
+                video = yt.streams.filter(only_audio=True).first()
+                # Download the video to a file
+                out_file = video.download(output_path=save_path)
+
+            except FileExistsError:
+                print("That file already exists.")
+
+            else:
+                # Set up new filename for the MP3
+                new_file = out_file.replace(".mp4", ".mp3")
+                # Use moviepy to convert video audio to mp3
+                video_clip = AudioFileClip(out_file)
+                video_clip.write_audiofile(new_file)
+                
+                # Remove the original video file
+                os.remove(out_file)
+                
+                url_entry.delete(0, tk.END)
 
         elif default_folder.get() == 0:
+            # Ask for a different save path
             new_save_path = askdirectory()
-            url = url_entry.get()
             # Use pytube to download the video
-            yt = YouTube(url)
-            video = yt.streams.filter(only_audio=True).first()
+            yt = YouTube(url_entry.get())
+
+            try:
+                video = yt.streams.filter(only_audio=True).first()
+                # Download the video to a file
+                out_file = video.download(output_path=new_save_path)
             
-            # Download the video to a file
-            out_file = video.download(output_path=new_save_path)
-            
-            # Set up new filename for the MP3
-            new_file = out_file.replace(".mp4", ".mp3")
-            # Use moviepy to convert video audio to mp3
-            video_clip = AudioFileClip(out_file)
-            video_clip.write_audiofile(new_file)
-            
-            # Remove the original video file
-            os.remove(out_file)
-            
-            url_entry.delete(0, tk.END)
+            except FileExistsError:
+                print("That file already exists.")
+
+            else:
+                # Set up new filename for the MP3
+                new_file = out_file.replace(".mp4", ".mp3")
+                # Use moviepy to convert video audio to mp3
+                video_clip = AudioFileClip(out_file)
+                video_clip.write_audiofile(new_file)
+                
+                # Remove the original video file
+                os.remove(out_file)
+                
+                url_entry.delete(0, tk.END)
 
     elif mp4_flag.get() == 1:
-        resolution = '2160p'
         if default_folder.get() == 1:
-            url = url_entry.get()
             # Use pytube to download the video
-            yt = YouTube(url)
+            yt = YouTube(url_entry.get())
             
-            audio = yt.streams.filter(only_audio=True).first().download(output_path=save_path)
-            aname = audio[:-4] + ".mp3"
-            os.rename(audio, aname)
+            try:
+                audio = yt.streams.filter(only_audio=True).first().download(output_path=save_path)
+                aname = audio[:-4] + ".mp3"
+                os.rename(audio, aname)
 
-            video = yt.streams.filter(subtype='mp4', res=resolution).first().download(output_path=save_path)
-            vname = video[:-4] + "1.mp4"
-            os.rename(video, vname)
-            
-            # Setting the audio to the video
-            video = mpe.VideoFileClip(vname)
-            audio = mpe.AudioFileClip(aname)
-            final = video.set_audio(audio)
-            # Output result
-            final.write_videofile(vname[:-5] + vname[-4:])
+                video = yt.streams.filter(subtype='mp4', res=resolution.get()).first().download(output_path=save_path)
+                vname = video[:-4] + "1.mp4"
+                os.rename(video, vname)
 
-            # Remove the original video file
-            os.remove(aname)
-            os.remove(vname)
+            except AttributeError:
+                print("That video does not have the requested resolution YouTube, please try a different one.")
+                os.remove(aname)
+
+            except FileExistsError:
+                print("That file already exists.")
             
-            url_entry.delete(0, tk.END)
+            else:
+                # Setting the audio to the video
+                video = mpe.VideoFileClip(vname)
+                audio = mpe.AudioFileClip(aname)
+                final = video.set_audio(audio)
+                # Output result
+                final.write_videofile(vname[:-5] + vname[-4:])
+
+                # Remove the original video file
+                os.remove(aname)
+                os.remove(vname)
+                
+                url_entry.delete(0, tk.END)
+            
 
         elif default_folder.get() == 0:
+            # Ask for a different save path
             new_save_path = askdirectory()
-            url = url_entry.get()
             # Use pytube to download the video
-            yt = YouTube(url)
+            yt = YouTube(url_entry.get())
             
-            audio = yt.streams.filter(only_audio=True).first().download(output_path=save_path)
-            aname = audio[:-4] + ".mp3"
-            os.rename(audio, aname)
+            try:
+                audio = yt.streams.filter(only_audio=True).first().download(output_path=save_path)
+                aname = audio[:-4] + ".mp3"
+                os.rename(audio, aname)
 
-            video = yt.streams.filter(subtype='mp4', res=resolution).first().download(output_path=new_save_path)
-            vname = video[:-4] + "1.mp4"
-            os.rename(video, vname)
-            
-            # Setting the audio to the video
-            video = mpe.VideoFileClip(vname)
-            audio = mpe.AudioFileClip(aname)
-            final = video.set_audio(audio)
-            # Output result
-            final.write_videofile(vname[:-5] + vname[-4:])
+                video = yt.streams.filter(subtype='mp4', res=resolution.get()).first().download(output_path=new_save_path)
+                vname = video[:-4] + "1.mp4"
+                os.rename(video, vname)
 
-            # Remove the original video file
-            os.remove(aname)
-            os.remove(vname)
+            except AttributeError:
+                print("That video does not have the requested resolution on YouTube, please try a different one.")
+                os.remove(aname)
+
+            except FileExistsError:
+                print("That file already exists.")
             
-            url_entry.delete(0, tk.END)
+            else:
+                # Setting the audio to the video
+                video = mpe.VideoFileClip(vname)
+                audio = mpe.AudioFileClip(aname)
+                final = video.set_audio(audio)
+                # Output result
+                final.write_videofile(vname[:-5] + vname[-4:])
+
+                # Remove the original video file
+                os.remove(aname)
+                os.remove(vname)
+                
+                url_entry.delete(0, tk.END)
 
 mp3_flag = tk.IntVar(window, value=1, name="mp3")
 mp4_flag = tk.IntVar(window, value=0, name="mp4")
@@ -119,6 +144,14 @@ def mp3_command():
     mp4_btn["relief"] = tk.RAISED
     mp4_btn["borderwidth"] = 2
     mp3_btn["borderwidth"] = 5
+    res_144p_btn.configure(state='disabled')
+    res_240p_btn.configure(state='disabled')
+    res_360p_btn.configure(state='disabled')
+    res_480p_btn.configure(state='disabled')
+    res_720p_btn.configure(state='disabled')
+    res_1080p_btn.configure(state='disabled')
+    res_1440p_btn.configure(state='disabled')
+    res_2160p_btn.configure(state='disabled')
  
 
 def mp4_command():
@@ -128,18 +161,28 @@ def mp4_command():
     mp3_btn["relief"] = tk.RAISED
     mp4_btn["borderwidth"] = 5
     mp3_btn["borderwidth"] = 2
+    res_144p_btn.configure(state='normal')
+    res_240p_btn.configure(state='normal')
+    res_360p_btn.configure(state='normal')
+    res_480p_btn.configure(state='normal')
+    res_720p_btn.configure(state='normal')
+    res_1080p_btn.configure(state='normal')
+    res_1440p_btn.configure(state='normal')
+    res_2160p_btn.configure(state='normal')
 
 
-url_entry_convert_btn_frame = tk.Frame()
+url_entry_convert_btn_frame = tk.Frame(master=window)
 #convert_btn_frame = tk.Frame()
-mp3_4_btn_frame = tk.Frame()
-default_folder_frame = tk.Frame()
+mp34_default_btns_frame = tk.Frame(master=window)
+mp3_4_btn_frame = tk.Frame(master=mp34_default_btns_frame)
+default_folder_frame = tk.Frame(master=mp34_default_btns_frame)
+resolution_btns_frame = tk.Frame(master=window)
+
 
 url_entry = tk.Entry(master=url_entry_convert_btn_frame,
                      width=50
                      )
 url_entry.pack(side=tk.LEFT)
-
 
 
 mp3_btn = tk.Button(master=mp3_4_btn_frame,
@@ -160,6 +203,7 @@ mp4_btn = tk.Button(master=mp3_4_btn_frame,
                     )
 mp4_btn.pack(side=tk.LEFT)
 
+
 convert_btn = tk.Button(master=url_entry_convert_btn_frame,
                         text="Convert",
                         width=10,
@@ -167,6 +211,7 @@ convert_btn = tk.Button(master=url_entry_convert_btn_frame,
                         command=convert_command
                         )
 convert_btn.pack(side=tk.RIGHT)
+
 
 default_folder = tk.IntVar(value=1)
 default_folder_btn = ttk.Checkbutton(master=default_folder_frame,
@@ -177,10 +222,71 @@ default_folder_btn = ttk.Checkbutton(master=default_folder_frame,
                                      )
 default_folder_btn.pack(side=tk.RIGHT)
 
+
+resolution = tk.StringVar(value='720p')
+res_144p_btn = ttk.Radiobutton(master=resolution_btns_frame,
+                               text='144p',
+                               variable=resolution,
+                               value='144p')
+res_144p_btn.pack(side=tk.TOP)
+res_144p_btn.configure(state='disabled')
+
+res_240p_btn = ttk.Radiobutton(master=resolution_btns_frame,
+                               text='240p',
+                               variable=resolution,
+                               value='240p')
+res_240p_btn.pack(side=tk.TOP)
+res_240p_btn.configure(state='disabled')
+
+res_360p_btn = ttk.Radiobutton(master=resolution_btns_frame,
+                               text='360p',
+                               variable=resolution,
+                               value='360p')
+res_360p_btn.pack(side=tk.TOP)
+res_360p_btn.configure(state='disabled')
+
+res_480p_btn = ttk.Radiobutton(master=resolution_btns_frame,
+                               text='480p',
+                               variable=resolution,
+                               value='480p')
+res_480p_btn.pack(side=tk.TOP)
+res_480p_btn.configure(state='disabled')
+
+res_720p_btn = ttk.Radiobutton(master=resolution_btns_frame,
+                               text='720p',
+                               variable=resolution,
+                               value='720p')
+res_720p_btn.pack(side=tk.TOP)
+res_720p_btn.configure(state='disabled')
+
+res_1080p_btn = ttk.Radiobutton(master=resolution_btns_frame,
+                               text='1080p',
+                               variable=resolution,
+                               value='1080p')
+res_1080p_btn.pack(side=tk.TOP)
+res_1080p_btn.configure(state='disabled')
+
+res_1440p_btn = ttk.Radiobutton(master=resolution_btns_frame,
+                               text='1440p',
+                               variable=resolution,
+                               value='1440p')
+res_1440p_btn.pack(side=tk.TOP)
+res_1440p_btn.configure(state='disabled')
+
+res_2160p_btn = ttk.Radiobutton(master=resolution_btns_frame,
+                               text='2160p',
+                               variable=resolution,
+                               value='2160p')
+res_2160p_btn.pack(side=tk.TOP)
+res_2160p_btn.configure(state='disabled')
+
+
 url_entry_convert_btn_frame.pack(side=tk.TOP)
 #convert_btn_frame.pack(side=tk.RIGHT)
 mp3_4_btn_frame.pack(side=tk.LEFT)
 default_folder_frame.pack(side=tk.RIGHT)
+mp34_default_btns_frame.pack(side=tk.TOP)
+resolution_btns_frame.pack(side=tk.TOP)
 
 # Start the event loop.
 window.mainloop()
